88b11e5995d9cef39e20d44569b547a043b4f941
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-03-31 23:21:12 +0300

    cat class with a method

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+//cat class
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+const cat = new Cat("Maya", "male");
+console.log(cat.speak());
+
+//dog class
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+}
+
+//bird class
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+}
